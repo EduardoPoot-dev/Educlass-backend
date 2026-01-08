a6151a780693c86c2b104f4ab5d13b37bd63ec35
Eduardo Poot <eduardopoot_@outlookk.com>
2026-01-07 18:38:33 -0600

    cors test 2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,12 +3,13 @@ import { CorsOptions } from "cors";
 export const corsConfig : CorsOptions = {
     origin: function(origin, callback) {
         const blackList = [process.env.FRONTEND_URL]
-
+        
         if(process.argv[2] === '--api') {
-            return blackList.push(undefined)
+            blackList.push(undefined)
         }
-
+console.log(blackList)
         if(blackList.includes(origin)) {
+            console.log(origin)
             callback(null, true)
         } else {
             callback(new Error('Error de cors'))
