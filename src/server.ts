1c826663364641d31f06c55a12ad43f984f9ec77
Eduardo Poot <eduardopoot_@outlookk.com>
2026-01-07 19:17:42 -0600

    test cors

--- src/server.ts
+++ src/server.ts
@@ -21,9 +21,9 @@ connectDb()
 
 const app = express()
 
-app.use(express.json());
-
 app.use(cors(corsConfig))
+
+app.use(express.json());
 
 app.use('/api/auth', authRouter)
 app.use('/api/course', courseRouter )
